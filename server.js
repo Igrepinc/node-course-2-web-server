01ffce316da7b3b909e45c8d8224bd6e2f0539ba
Igor <igrepinc@gmail.com>
2018-10-03 12:34:29 +0200

    Add project page

--- server.js
+++ server.js
@@ -18,9 +18,11 @@ app.use((req, res, next) => {
    next();
 });
 
+/*
 app.use((req, res, next) => {
     res.render('maintenance.hbs');
 });
+*/
 
 app.use(express.static(__dirname + '/public'));
 
@@ -41,6 +43,12 @@ app.get('/', (req, res) => {
 
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
+        pageTitle: 'About page'
+    });
+});
+
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
         pageTitle: 'About page'
     });
 });
